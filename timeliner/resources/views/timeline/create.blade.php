bd3173e663fb749609e28d55804484358f4ee387
mael brandt <mael.brandt@he-arc.ch>
2024-10-24 10:20:14 +0200

    added create timeline form v1

--- timeliner/resources/views/timeline/create.blade.php
+++ timeliner/resources/views/timeline/create.blade.php
@@ -1,13 +1,14 @@
-<!-- TODO: add partial/layout -->
+
+@extends('layout.app')
 
 @section('content')
 <div class="row mb-3">
     <div class="col-12">
-        <a class="btn btn-primary" href={{ route('/') }}> Retour</a>
+        <a class="btn btn-primary" href="TODO timeline.index"> Retour</a>
     </div>
 </div>
 
-<form action="{{ route("books.store") }}" method="POST">
+<form action="TODO" method="POST">
     @csrf
 
     <div class="row">
@@ -20,29 +21,21 @@
                     <div class="form-row">
                         <div class="form-group col-12">
                             <label for="inputTitle">Titre</label>
-                            <input type="text" name="title" value="{{old('title')}}" class="form-control" id="inputTitle">
-                        </div>
-                        <div class="form-group col-12">
-                            <label for="exampleFormControlSelect1">Auteur</label>
-                            <select class="form-control" name="author_id" id="exampleFormControlSelect1">
-                                <option value="">Auteur inconnu...</option>
-                                @foreach ($authors as $author)
-                                    <option value="{{$author->id}}" {{ (old("author_id") == $author->id ? "selected":"") }}>{{$author->name}}</option>
-                                @endforeach
-                            </select>
+                            <input type="text" name="title" class="form-control" id="inputTitle">
                         </div>
                         <div class="row mt-3">
                             <div class="form-group col-6">
-                                <label for="inputPages">Nombre de pages</label>
-                                <input type="text" name="pages" value="{{old('pages')}}" class="form-control" id="inputPages">
+                                <label for="selectStartDate">Date de départ</label>
+                                <input type="date" name="startDate" value="{{date('d/m/y')}}" class="form-control" id="selectStartDate">
                             </div>
                             <div class="form-group col-6">
-                                <label for="inputQuantity">Quantité</label>
-                                <input type="text" name="quantity" value="{{old('quantity')}}" class="form-control" id="inputQuantity">
+                                <label for="selectEndDate">Date de fin</label>
+                                <input type="date" name="endDate" value="{{date('d/m/y')}}" class="form-control" id="selectEndDate">
                             </div>
                         </div>
-
-                        <button type="submit" class="btn btn-primary mt-3">Envoyer</button>
+                        <div class="form-group col-6">
+                            <button class="btn btn-primary" href="TODO timeline.edit/newly created timeline" type="submit" class="btn btn-primary mt-3">Terminer</button>
+                        </div>
                     </div>
                 </div>
             </div>
