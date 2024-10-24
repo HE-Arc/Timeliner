39fe6452d1339c1cc2a30628440ee360a3fa0841
Theory-of-everything1 <58880827+theory-of-evrth@users.noreply.github.com>
2024-10-24 11:23:51 +0300

    provider edit (Elisa's)

--- timeliner/app/Providers/AppServiceProvider.php
+++ timeliner/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
 
 class AppServiceProvider extends ServiceProvider
 {
