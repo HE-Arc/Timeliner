a7543f9fe763222dd5eefbe2b6a906cb5bf9e2c9
mael brandt <mael.brandt@he-arc.ch>
2024-10-29 08:41:31 +0100

    created methode timeline.store

--- timeliner/app/Http/Controllers/TimelineController.php
+++ timeliner/app/Http/Controllers/TimelineController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Timeline;
+use Illuminate\Http\Request;
+
 class TimelineController extends Controller
 {
     public function fetchAllAvailable()
     {
         return view('index', ['timelines'=>[["Timeline1:title",false,"somedescription"],["Timeline2",true,"some other description"]]]);
     }
-    
+
     public function timelinelist()
     {
         return view('timeline.timelinelist');
@@ -17,5 +20,12 @@ class TimelineController extends Controller
     public function create()
     {
         return view('timeline.create');
+    }
+
+    public function store(Request $request)
+    {
+        \App\Models\Timeline::create($request->all());
+
+        return redirect()->route('timeline.timelinelist')->with('success','Timeline created successfully.');
     }
 }
