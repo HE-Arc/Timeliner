b128f35d451bebd0a14ce3e972c2fbb496a7fe87
Elisa Goloviatinski <elisa.goloviatinski@he-arc.ch>
2024-10-30 00:36:34 +0100

    Added link to registration page from login

--- timeliner/resources/views/about.blade.php
+++ timeliner/resources/views/about.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("All rights reserved") }}
-                    <p>Made in He-Arc during the course DevWebI</p>
+                    <p>Made in HE-Arc during the DevWebI course</p>
                 </div>
             </div>
         </div>
