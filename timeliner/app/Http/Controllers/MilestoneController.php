9d74c98d4eb800f618183e16cf251705e1685e4c
mael brandt <mael.brandt@he-arc.ch>
2024-12-05 10:19:41 +0100

    added created mileston list

--- timeliner/app/Http/Controllers/MilestoneController.php
+++ timeliner/app/Http/Controllers/MilestoneController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Milestone;
+use App\Models\Timeline;
 use Illuminate\Http\Request;
 
 class MilestoneController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('timeline.partials.milestonecreate');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'date' => 'required',
+            'description' => 'required|max:200'
+        ]);
+
+        $milestone = Milestone::create($request->all());
+    }
+
+    public function addToList()
+    {
+        
+    }
 }
--- timeliner/resources/views/timeline/partials/milestonecreate.blade.php
+++ timeliner/resources/views/timeline/partials/milestonecreate.blade.php
@@ -1,14 +1,40 @@
+
 <form> <!-- action="route("milestone.store")..." -->
     @csrf
     <div class="form-group col-12">
         <div class="form-group col-6">
             <label for="milestoneDate">due date</label>
-            <input type="date" name="datePicker" class="form-control" id="milestoneDate">
+            <input type="date" id="milestone-datePicker" name="datePicker" class="form-control" id="milestoneDate">
             <label for="milestoneDesc">Description</label>
-            <input type="text" name="Decription" class="form-control" id="milestoneDesc">
+            <input type="text" id="milestone-description-field" name="Decription" class="form-control" id="milestoneDesc">
         </div>
         <!-- href= route('milestone.create') -->
-            <button class="btn btn-primary" type="button" class="btn btn-primary mt-3">Create Milestone</button>
+            <button class="btn btn-primary" id="milestone-create-button" type="button" class="btn btn-primary mt-3">Create Milestone</button>
     </div>
-    <!-- TODO add milestone liste + delete -->
+    <!-- TODO delete milestone -->
+    <div id="milestone-list">
+        <tr>
+            <th>Description</th>
+            <th>Date</th>
+        </tr>
+    </div>
+    <script>
+        document.getElementById("milestone-create-button").addEventListener('click', function() {
+
+            let tr = document.createElement("tr");
+            let td_date = document.createElement("td");
+            let td_desc = document.createElement("td");
+
+            let date = document.getElementById('milestone-datePicker').value;
+            let description = document.getElementById('milestone-description-field').value;
+
+            td_date.append(date)
+            td_desc.append(description)
+
+            tr.append(td_desc)
+            tr.append(td_date)
+
+            document.getElementById("milestone-list").append(tr);
+        });
+    </script>
 </form>
